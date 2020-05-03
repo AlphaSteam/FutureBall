5f3273c697f05b1d521d4a62f1a856a6b80f19fc
Ariel Nuñez Lobos <54522577+Marmotato@users.noreply.github.com>
2020-05-03 15:22:25 -0400

    Add files via upload

---
+++ platformer/Ball/Scripts/Prueba.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+
+var touched=false 
+func set_touched():
+	touched=true
+
+func _on_GenericBall_body_entered(body):
+	if touched==false:
+		print(str(self)+" hits "+str(body))
+		get_stream_playback()
+
+	var other_node=body.get_owner().get_node(body.get_name())
+	if other_node.has_method("set_touched"):
+		other_node.set_touched()
+
+func _on_GenericBall_body_exited(body):
+	touched=false
