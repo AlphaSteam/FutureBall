b7ca8f494a5ef8ba4f07c1e2e44c34e755ac764f
Sebastian Alfaro <sebastian.alfaro@ing.uchile.cl>
2020-05-07 08:10:42 -0400

    More jumps available
    -A player can be set to have multiple jumps, which means he will be able to jump in mid air if it has available jumps left.
    There's a threshold for how much time will have the player to do the next jump, after this time runs out, the player loses all his jumps and must fall (Can still dash).
    Dashing resets this threshold, so one can jump, dash and jump again if done quickly enough.
    When the player lands it regains all his jumps.

--- platformer/Player.gd
+++ platformer/Player.gd
@@ -7,6 +7,7 @@ const GRAVITY = 10
 const JUMP_POWER = -250
 const FLOOR = Vector2(0,-1)
 const DRAG = 10
+const INITJUMPS =3
 
 var velocity = Vector2(0,0)
 var on_ground = false
@@ -16,9 +17,14 @@ var speed = DEFSPEED
 var friction = DEFFRICTION
 var acceleration = DEFACC
 var cooldown = false
-var double_jump = true # Do I have a second jump available?
+var jumps_available = INITJUMPS # Do I have jumps available?
 var end_jump = false
 var end_jump_after = false
+
+
+
+	
+
 func _physics_process(delta):
 	var input_velocity = Vector2.ZERO
 	if Input.is_action_pressed("Right"):
@@ -26,41 +32,51 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("Dash"):
 			
 			_dash()
-		
+			
 	if Input.is_action_pressed("Left"):
 		input_velocity.x -= 1
 		if Input.is_action_just_pressed("Dash"):
 			_dash()
 	
-		
-	
 	if Input.is_action_just_pressed("Up"):
-		if on_ground == true:
+		
+		if jumps_available > 0:
+			
+			#if on_ground == true:
+			jumps_available = jumps_available - 1
+			
 			velocity.y = JUMP_POWER
 			on_ground = false
 			end_jump = false
+			$"New jump threshold".start()
 			$"Minimun jump duration".start()
+			
+		else:
+			print("test")
 			
 	if(dash == false):		
 		velocity.y += GRAVITY
 	
 	
-	 
+	
+	
+		
 	if velocity.y < 0 && Input.is_action_just_released("Up") && end_jump == true:
 		_end_jump()
+		
 	elif velocity.y < 0 && Input.is_action_just_released("Up") && end_jump == false:
 		end_jump_after = true
 	
 	if is_on_floor():
+		jumps_available = INITJUMPS
 		on_ground = true
 		if(dash== false):
 			speed = DEFSPEED
 		
 	else:
 		on_ground = false
-		
 		if(dash == false):
-			speed=4*DEFSPEED/5
+			speed=4.0*DEFSPEED/6
 		
 	input_velocity = input_velocity.normalized() * speed
 	if input_velocity.length() > 0:
@@ -70,10 +86,12 @@ func _physics_process(delta):
 		velocity.x = velocity.linear_interpolate(Vector2.ZERO, friction).x
 	#print(velocity)
 	velocity = move_and_slide(velocity,FLOOR)
+	print(jumps_available)
 	
 func _end_jump():
 	velocity.y += - velocity.y*0.98#Jump Height depends on how long you will hold key
 	end_jump = false
+
 
 func _dash():
 	if(cooldown == false):
@@ -82,16 +100,17 @@ func _dash():
 		velocity.y = 0
 		dash = true
 		cooldown = true
-		
+		$"New jump threshold".start()
+		$"New jump threshold".paused = true
 		$"Dash Time".start()
 
 
 
 func _on_Dash_Time_timeout():
 	dash = false
-	#speed = DEFSPEED * 3  + (DEFSPEED  - DEFSPEED * 3) * 0
 	acceleration = DEFACC
 	friction = DEFFRICTION
+	$"New jump threshold".paused = false
 	$"Dash Cooldown".start()
 
 func _on_Dash_Cooldown_timeout():
@@ -100,8 +119,12 @@ func _on_Dash_Cooldown_timeout():
 
 
 func _on_Minimun_jump_duration_timeout():
+	print("test 2")
 	end_jump = true
-	
 	if (end_jump_after):
 		_end_jump()
 		end_jump_after= false
+
+
+func _on_New_jump_threshold_timeout():
+	jumps_available = 0
