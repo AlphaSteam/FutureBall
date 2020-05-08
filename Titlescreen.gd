01dd13ddcb7a8a6a36375399cabefbaf94a36771
Diego Sandoval <daslwebmaster@gmail.com>
2020-05-08 00:43:26 -0400

    menu

---
+++ Titlescreen.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+
+func _physics_process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton.is_hovered() ==true:
+		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("res://MainStage.tscn")
