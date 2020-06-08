8c4ab19f54c311c3a813f3592f6b8cf90b54ea0e
ArielNunezLobos <54522577+ArielNunezLobos@users.noreply.github.com>
2020-06-08 06:19:56 -0400

    fixed esc bug and added temporary winscreen on progress
    -fixed a bug where pressing esc would've crashed the game once it started
    -added a temporary winscreen after a player reaches either 5 points or -3 points.
    
    need to: make the counters be more like the UI, add signals with each players score for the winscreen

--- Score0.gd
+++ Score0.gd
@@ -10,14 +10,22 @@ extends Label
 func _ready():
 	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta):
+	if int(text) == 5 or int(text) == -3:
+		get_tree().change_scene("res://Winscreen.tscn")
+
 func _on_Bola_Punto0():
 	text = str(int(text) + 1)
 	pass # Replace with function body.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Player0_sd0():
 	text = str(int(text) - 1)
 	pass # Replace with function body.
+	
+
+#func _victory():
+#	if int(text) == 5 or int(text) == -3:
+#		get_tree().change_scene("res://Winscreen.tscn")
