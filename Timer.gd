993b1dbebbd44a7175e2732525f95f3a971bad75
Diego Sandoval <daslwebmaster@gmail.com>
2020-07-27 14:04:03 -0400

    Timer
    Hice un timer pero le falta afectar a su entorno

---
+++ Timer.gd
@@ -0,0 +1,23 @@
+extends Label
+
+var round_time=40
+onready var timer = get_node("Timer")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.set_wait_time(round_time)
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	set_text(str(int(timer.time_left)))
+
+
+
+func _on_Timer_timeout():
+	print("hola") # Replace with function body.
