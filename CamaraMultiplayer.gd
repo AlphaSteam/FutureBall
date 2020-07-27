c1793ba6dba1891f6713edc74b53285d99f9cf25
Diego Sandoval <daslwebmaster@gmail.com>
2020-07-27 13:28:14 -0400

    Personaje y marcador FIXED
    Ya no se superponen el marcador y los personajes

--- CamaraMultiplayer.gd
+++ CamaraMultiplayer.gd
@@ -13,7 +13,7 @@ func _process(delta):
 	self.global_position = (object1.global_position + object2.global_position) * 0.5
 
 	var zoom_factor1 = abs(object1.global_position.x-object2.global_position.x)/(200)
-	var zoom_factor2 = abs(object1.global_position.y-object2.global_position.y)/(200)
+	var zoom_factor2 = abs(object1.global_position.y-object2.global_position.y-20)/(200)
 	var zoom_factor = max(max(zoom_factor1, zoom_factor2), 1.2)
 
 	self.zoom = Vector2(zoom_factor*1, zoom_factor*1)
