e6397f1fc903aa123850b930406dcc12f864234b
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2020-07-29 22:57:51 -0400

    Fixed player and ball duplicating on new game

--- Global_vars.gd
+++ Global_vars.gd
@@ -5,7 +5,7 @@ const GRAVITY = 10
 const JUMP_POWER = -250
 const FLOOR = Vector2(0,-1)
 var Winner
-
+var ball
 
 func _ready():
 	pass
@@ -15,4 +15,18 @@ func _process(delta):
 func WinScreen(var player):
 	Winner = player
 	get_tree().change_scene("res://Winscreen.tscn")
+func spawnBall():
+	for i in get_tree().get_nodes_in_group("Ball"):
+		i.queue_free()
+	ball = preload("res://Bola.tscn").instance()
+	var ball_spawns = get_tree().get_nodes_in_group("Ball spawn")
+	var length = ball_spawns.size()
+	var a = []
+	for j in range(length):
+		a.append(j) 
+	var random = randi()%a.size()
+	var spawn_p = ball_spawns[a[random]].get_global_position()
+	
+	ball.global_position = spawn_p
+	add_child(ball)
 
