41984921106d602dc39a2d82d9c67d5ff86f82ba
ArielNunezLobos <54522577+ArielNunezLobos@users.noreply.github.com>
2020-07-30 19:46:29 -0400

    Música y options
    Options maneja la música
    Música en cada stage, en bus 1
    SFX en bus 2 (no hay niun sfx hasta ahora)

---
+++ CheckBoxSFX.gd
@@ -0,0 +1,25 @@
+extends CheckButton
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var estado_init = true
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CheckBoxSFX_toggled(button_pressed):
+	var estado_music = not button_pressed 
+	AudioServer.set_bus_mute(2, estado_music)
+
+	
+func _ready():
+	pressed = not AudioServer.is_bus_mute(2)
